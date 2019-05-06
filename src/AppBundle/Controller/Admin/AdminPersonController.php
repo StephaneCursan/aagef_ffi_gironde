9f27a1a1b0119d0b0f269de6384428b621ca352a
lapiscine <stephane.cursan@lapiscine.pro>
2019-05-07 00:21:10 +0200

    Implementation (to be finalized) of a form 'PersonType' and a controller 'AdminPersonController' to allow the addition of peoples in the Person table of the database

---
+++ src/AppBundle/Controller/Admin/AdminPersonController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lapiscine
+ * Date: 2019-05-06
+ * Time: 20:36
+ */
+
+namespace AppBundle\Controller\Admin;
+
+
+class AdminPersonController
+{
+
+}
---
+++ src/AppBundle/Form/PersonType.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lapiscine
+ * Date: 2019-05-06
+ * Time: 21:21
+ */
+
+namespace AppBundle\Form;
+
+
+class PersonType
+{
+
+}
