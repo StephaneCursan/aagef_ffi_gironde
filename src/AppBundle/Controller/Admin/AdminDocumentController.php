db59b46f528587a7133751677f22e8ac0ffa82b2
lapiscine <stephane.cursan@lapiscine.pro>
2019-05-16 20:24:33 +0200

    Creation of the Route to display all the documents and another one to display only the one selected. Another one allow the display of all persons. Finally, creation of the last four Route (two about Documents and two about Persons) that manage the updating and deletion of rows in the DB.

--- src/AppBundle/Controller/Admin/AdminDocumentController.php
+++ src/AppBundle/Controller/Admin/AdminDocumentController.php
@@ -12,6 +12,7 @@ use AppBundle\Entity\Document;
 use AppBundle\Form\DocumentType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,6 +20,38 @@ class AdminDocumentController extends Controller
 {
     /**
      * @Route("Admin/database/documents", name="documents")
+     */
+    public function documentListAction()
+    {
+        $documentList = $this->getDoctrine()
+            ->getRepository(Document::class)
+            ->findAll();
+
+        return $this->render('Admin/listDocument.html.twig',
+            [
+                'documentList' => $documentList
+            ]
+        );
+    }
+
+    /**
+     * @Route("/database/documentSingle/{id}", name="documentSingle")
+     */
+    public function documentSingleAction($id)
+    {
+        $documentRepository = $this->getDoctrine()
+            ->getRepository(Document::class);
+        $documentSingle = $documentRepository->find($id);
+
+        return $this->render('Admin//singleDocument.html.twig',
+            [
+                'documentSingle' => $documentSingle
+            ]
+        );
+    }
+
+    /**
+     * @Route("Admin/database/create_document", name="create_document")
      */
     public function DocumentCreateAction(Request $request)
     {
@@ -59,5 +92,72 @@ class AdminDocumentController extends Controller
                 'documentFormView' => $documentFormView
             ]
         );
+    }
+
+    /**
+     * @Route("Admin/database/document_update/{id}", name="document_update")
+     */
+    public function personUpdateAction($id)
+    {
+        $request = Request::createFromGlobals();
+
+        $documentRepository = $this->getDoctrine()->getRepository(Document::class);
+        $document = $documentRepository->find($id);
+
+        $oldFile = new File($this->getParameter('upload_files_document').$document->getPhoto());
+        $document->setPhoto($oldFile);
+
+        $documentForm = $this->createForm(documentType::class, $document);
+
+        $documentFormView = $documentForm->createView();
+
+        $documentForm->handleRequest($request);
+
+        if ($documentForm->isSubmitted() && $documentForm->isValid()){
+
+            $file = $document->getPhoto();
+            $fileName = md5(uniqid()).'.'.$file->guessExtension();
+
+            try {
+                $file->move(
+                    $this->getParameter('upload_files_document'),
+                    $fileName
+                );
+            } catch (FileException $e){
+                throw new \Exception($e->getMessage());
+            }
+
+            $document->setPhoto($fileName);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($document);
+            $entityManager->flush();
+
+            var_dump("les données du document ont été mises à jour"); die;
+        }
+
+        return $this->render('Admin/updateDocument.html.twig',
+            [
+                'documentFormView' => $documentFormView
+            ]
+        );
+    }
+
+    /**
+     * @Route("Admin/database/document_delete/{id}", name="document_delete")
+     */
+    public function documentDeleteAction($id)
+    {
+        $documentRepository = $this->getDoctrine()
+            ->getRepository(Document::class);
+        $document = $documentRepository->find($id);
+
+        $entityManager = $this->getDoctrine()
+            ->getManager();
+
+        $entityManager->remove($document);
+        $entityManager->flush();
+
+        var_dump("Un enregistrement de la table Document a été supprimé"); die;
     }
 }
