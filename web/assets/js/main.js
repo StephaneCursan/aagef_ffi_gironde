2badace767448ef2281826f49046baea4a1431d1
lapiscine <stephane.cursan@lapiscine.pro>
2019-06-10 11:30:28 +0200

    Implementation of a back to top button.

---
+++ web/assets/js/main.js
@@ -0,0 +1,17 @@
+/*** Back to top button display ***/
+
+$(window).scroll(function () {
+
+    if ($(this).scrollTop() > 100) {
+        $('#backToTop').fadeIn();
+    } else {
+        $('#backToTop').fadeOut();
+    }
+
+});
+
+/*** Click event scroll to top button jQuery ***/
+
+$('#backToTop').click(function () {
+    $('html, body').animate({scrollTop : 0}, 600);
+});
